28ca66fcd81f22dcf3f81d05837793109af301ae
Vladimir <v.petrzhikovskiy@dexpa.io>
2021-08-13 19:03:35 +0300

    Fix errors

--- src/error.rs
+++ src/error.rs
@@ -16,10 +16,10 @@ use ton_types::types::ExceptionCode;
 #[derive(Debug, thiserror::Error)]
 pub enum BlockError {
     /// Fatal error.
-    #[error("Fatal error: {}", 0)]
+    #[error("Fatal error: {0}")]
     FatalError(String),
     /// Invalid argument.
-    #[error("Invalid argument: {}", 0)]
+    #[error("Invalid argument: {0}")]
     InvalidArg(String),
     /// Invalid TL-B constructor tag.
     #[error("Invalid TL-B constructor tag `#{:x}` while parsing `{}` struct", t, s)]
@@ -28,34 +28,34 @@ pub enum BlockError {
         s: String,
     },
     /// Invalid data.
-    #[error("Invalid data: {}", 0)]
+    #[error("Invalid data: {0}")]
     InvalidData(String),
     /// Invalid index.
-    #[error("Invalid index: {}", 0)]
+    #[error("Invalid index: {0}")]
     InvalidIndex(usize),
     /// Invalid operation.
-    #[error("Invalid operation: {}", 0)]
+    #[error("Invalid operation: {0}")]
     InvalidOperation(String),
     /// Item is not found.
-    #[error("{} is not found", 0)]
+    #[error("{0} is not found")]
     NotFound(String),
     /// Other error.
-    #[error("{}", 0)]
+    #[error("{0}")]
     Other(String),
     /// Attempting to read data from pruned branch cell.
-    #[error("Attempting to read {} from pruned branch cell", 0)]
+    #[error("Attempting to read {0} from pruned branch cell")]
     PrunedCellAccess(String),
     /// TVM Exception
-    #[error("VM Exception, code: {}", 0)]
+    #[error("VM Exception, code: {0}")]
     TvmException(ExceptionCode),
     /// Wrong hash.
     #[error("Wrong hash")]
     WrongHash,
     /// Wrong merkle proof.
-    #[error("Wrong merkle proof: {}", 0)]
+    #[error("Wrong merkle proof: {0}")]
     WrongMerkleProof(String),
     /// Wrong merkle update.
-    #[error("Wrong merkle update: {}", 0)]
+    #[error("Wrong merkle update: {0}")]
     WrongMerkleUpdate(String),
     #[error("Bad signature")]
     BadSignature,
